b019830fbd6408d420614e75b1a79cb7eb052e55
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-25 10:39:08 +0200

    boxes project: first commit

---
+++ boxes/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          It's-a me, Samu!
+        </p>
+    </div>
+  );
+}
+
+export default App;
