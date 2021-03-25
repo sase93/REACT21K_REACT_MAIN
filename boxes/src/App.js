f4d0f37a010a7e7ed072575406351002f20cefc5
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-25 13:11:45 +0200

    First components complete

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Title: {props.title}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          It's-a me, Samu!
-        </p>
+    <div>
+      <header>
+        <h1>Logo</h1>
+      </header>
+      <main>
+        <Box name="First Guy" age="30" title="Boss"/>
+        <Box name="First Gal" age="25" title="Manager"/>
+        <Box name="2nd Guy" age="20" title="Worker"/>
+      </main>
+      <footer>
+        <p>Copyright</p>
+      </footer>
     </div>
   );
 }
