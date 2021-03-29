24009beb1f866eab571f0ea4d4b86d0f2de0ddac
Samu <77488879+sase93@users.noreply.github.com>
2021-03-29 12:42:21 +0300

    Made some buttons, did things with class components

--- boxes/src/Main.js
+++ boxes/src/Main.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
 const Box = (props) => {
-    return (
-      <div className="box">
-        <h2>Name: {props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Title: {props.title}</p>
-      </div>
-    );
+  function consoleLog() {
+    console.log('sup');
+  };
+
+  return (
+    <div className="box">
+      <h2>Name: {props.name}</h2>
+      <p>Position: {props.position}</p>
+      <p>Main attribute: {props.attribute}</p>
+      <button onClick={consoleLog}>Click me</button>
+    </div>
+  );
 };
 
-const Main = () => {
+class Main extends Component {
+  state =  {
+    persons : [
+      {name: "Bristleback", position: 3, attribute: "STR"},
+      {name: "Anti-Mage", position: 1, attribute: "AGI"},
+      {name: "Witch Doctor", position: 5, attribute: "INT"}
+    ]
+  };
+
+  otherConsoleLog = () => {
+    console.log('ey');
+  };
+
+  render() {
     return (
-        <div>
-            <Box name="First Guy" age="30" title="Boss"/>
-            <Box name="First Gal" age="25" title="Manager"/>
-            <Box name="Second Guy" age="20" title="Worker"/>
-        </div>
-    );
-};
+      <div>
+          <button onClick={this.otherConsoleLog}>CLICK ME!</button>
+          <Box name={this.state.persons[0].name} position={this.state.persons[0].position} attribute={this.state.persons[0].attribute}/>
+          <Box name={this.state.persons[1].name} position={this.state.persons[1].position} attribute={this.state.persons[1].attribute}/>
+          <Box name={this.state.persons[2].name} position={this.state.persons[2].position} attribute={this.state.persons[2].attribute}/>
+      </div>
+  );
+  }
+}
 
 export default Main;
