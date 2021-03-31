6bc1f2c2ba86bca99bd38596e183ad17b563fd10
Samu <77488879+sase93@users.noreply.github.com>
2021-03-31 17:08:13 +0300

    Changed prior 'class component' to an actual class component, edited index.css and App.css stylings slightly

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Test!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="circle">0</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
