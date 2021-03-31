5144eb26ed02c47ea13778c280f9a0672e846433
Samu <77488879+sase93@users.noreply.github.com>
2021-03-31 17:20:22 +0300

    Additions to component: state, and buttons to increase decrease and reset the counter.

--- counter/src/App.js
+++ counter/src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counter : 0
+  }
+
+  increaseHandler = () => {
+    this.setState({
+      counter : this.state.counter + 1
+    });
+  };
+
+  decreaseHandler = () => {
+    this.setState({
+      counter : this.state.counter - 1
+    });
+  };
+
+  resetHandler = () => {
+    this.setState({
+      counter : 0
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1 className="circle">0</h1>
+        <h1 className="circle">{this.state.counter}</h1>
+        <button onClick={this.increaseHandler}>+</button>
+        <button onClick={this.decreaseHandler}>-</button>
+        <button onClick={this.resetHandler}>0</button>
       </div>
     );
   }
